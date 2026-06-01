23038ce94549eafec53089f4b379afe12d1cfa26
Jan-Pakos <janpakos123@gmail.com>
2026-06-01 15:41:16 +0200

    feat: add AppointmentList component with template and styles

---
+++ src/app/appointment-list/appointment-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-list',
+  imports: [],
+  templateUrl: './appointment-list.html',
+  styleUrl: './appointment-list.scss',
+})
+export class AppointmentList {
+
+  appointment = "TESTING";
+}
