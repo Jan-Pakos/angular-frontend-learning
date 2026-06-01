93aa80a6ed44a06978fc9dda82b6359843142ed7
Jan-Pakos <janpakos123@gmail.com>
2026-06-01 16:49:35 +0200

    feat: implement appointment creation functionality with input fields and list display

--- src/app/appointment-list/appointment-list.ts
+++ src/app/appointment-list/appointment-list.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 import { Appointment } from './models/appointment-list.model';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-appointment-list',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './appointment-list.html',
   styleUrl: './appointment-list.scss',
 })
 export class AppointmentList {
 
-  appointment : Appointment = {
-    id: 1,
-    title: "TESTING",
-    date: new Date('2024-06-01')
-  };
+  newAppointmentTitle : string = '';
+  newAppointmentDate : Date = new Date();
+
+  appointments : Appointment[] = []
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim() && this.newAppointmentDate) {
+      let newAppointment : Appointment = {
+        id : this.appointments.length + 1,
+        title : this.newAppointmentTitle,
+        date : this.newAppointmentDate
+      }
+      this.appointments.push(newAppointment);
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+    }
+  }
 }
